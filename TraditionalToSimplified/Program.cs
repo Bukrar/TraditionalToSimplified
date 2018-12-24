3d2cca1a7b80f4f1f2b9d55043bea1003f6586e0
Igor <igor@miniasp.com>
2018-12-24 09:55:58 +0800

    初版

--- TraditionalToSimplified/Program.cs
+++ TraditionalToSimplified/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.International.Converters.TraditionalChineseToSimplifiedConverter;
+using MySql.Data.MySqlClient;
+using System;
+using System.IO;
+using System.Text;
+using System.Web;
 
 namespace TraditionalToSimplified
 {
@@ -6,7 +11,66 @@ namespace TraditionalToSimplified
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connectString = "server=127.0.0.1;uid=root;pwd=H1242457411qaz;database=db_event_tw";
+            string showTableSql = "SHOW TABLES;";
+            string sqlSelectSql = "SELECT * FROM db_tw.my_country_tw;";
+
+            MySqlConnection mySqlConnection = new MySqlConnection(connectString);
+
+            // 連線到資料庫
+            try
+            {
+                mySqlConnection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 1045:
+                        Console.WriteLine("使用者帳號或密碼錯誤");
+                        break;
+                    default:
+                        Console.WriteLine("無法連線到資料庫.");
+                        break;
+                }
+            }
+
+            try
+            {
+                using (MySqlCommand mySqlcommand = new MySqlCommand(showTableSql, mySqlConnection))
+                {
+                    StreamWriter sw = new StreamWriter(@"D:\secret_plan.txt");
+                    MySqlDataReader myData = mySqlcommand.ExecuteReader();
+
+                    if (!myData.HasRows)
+                    {
+                        Console.WriteLine("No data.");
+                    }
+                    else
+                    {
+                        while (myData.Read())
+                        {
+                            string traditionalContent = HttpUtility.UrlDecode(myData.GetString(1), Encoding.GetEncoding("big5"));
+                            Console.WriteLine(traditionalContent);
+                            sw.WriteLine(traditionalContent);
+                            string simplifiedContent = Utility.ToSimplified(traditionalContent, "ToSimplified");
+                            Console.WriteLine(simplifiedContent);
+                            sw.WriteLine(simplifiedContent);
+                            
+                        }
+                        sw.Close();
+                        myData.Close();
+                    }
+                    Console.ReadLine();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error " + ex.Number + " : " + ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
+
+
---
+++ TraditionalToSimplified/Utility.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.International.Converters.TraditionalChineseToSimplifiedConverter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraditionalToSimplified
+{
+    public class Utility
+    {
+        //繁簡轉換Funtion,參數 Language 為 Big5 則轉繁體、GB2312 則轉簡體，其他狀況則輸出原字串
+        public static string ToSimplified(string SourceString, string Language)
+        {
+            string newString = string.Empty;
+            switch (Language)
+            {
+                case "ToTraditional":
+                    newString = ChineseConverter.Convert(SourceString, ChineseConversionDirection.SimplifiedToTraditional);
+                    break;
+                case "ToSimplified":
+                    newString = ChineseConverter.Convert(SourceString, ChineseConversionDirection.TraditionalToSimplified);
+                    break;
+                default:
+                    newString = SourceString;
+                    break;
+            }
+            return newString;
+        }
+    }
+}
