c7a9b6c6fd6fd94f61b08f3709a1345408c9dc2f
Igor <igor@miniasp.com>
2019-01-07 16:06:50 +0800

    no message

--- TraditionalToSimplified/Program.cs
+++ TraditionalToSimplified/Program.cs
@@ -1,28 +1,50 @@
-﻿using System;
+﻿using Duotify;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
 
 namespace TraditionalToSimplified
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            TraditionalToSimplified traditionalToSimplified = new TraditionalToSimplified();
-            //計時
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-
-            //執行修改繁體資料庫資料至簡體資料庫(繁體BIG5轉GB18030)
-            traditionalToSimplified.DbDataHandle();
-           
-            stopWatch.Stop();
-            TimeSpan ts = stopWatch.Elapsed;
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                                 ts.Hours, ts.Minutes, ts.Seconds,
-                                 ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
-            Console.ReadLine();
+        {              
+            using (var serviceProvider = ConfigureServices(new ServiceCollection()).BuildServiceProvider())
+            {
+                var timer = serviceProvider.GetService<Stopwatch>();
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();               
+                var traditionalToSimplified = serviceProvider.GetService<TraditionalToSimplified>();
+                //計時
+                timer.Start();
+                //執行修改繁體資料庫資料至簡體資料庫(繁體BIG5轉GB18030)
+                traditionalToSimplified.DbDataHandle();
+                timer.Stop();
+                TimeSpan ts = timer.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                     ts.Hours, ts.Minutes, ts.Seconds,
+                                     ts.Milliseconds / 10);
+                Console.WriteLine("RunTime " + elapsedTime);
+            }
+            
+            Console.ReadLine();  
         }
+
+        private static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            return services
+                .AddLogging(builder =>
+                {
+                    builder.AddConsole(opt => opt.IncludeScopes = true);
+                })
+                .AddTransient<HanConvert>()
+                .AddSingleton<TraditionalToSimplified>()
+                .AddSingleton<Stopwatch>()
+                .AddSingleton<Utility>();
+        }
+
     }
 }
 
