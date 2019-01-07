5283c7df227c7c6c71f226dcc6cb57f045b6029a
Igor <igor@miniasp.com>
2019-01-07 12:14:14 +0800

    no message

--- TraditionalToSimplified/Program.cs
+++ TraditionalToSimplified/Program.cs
@@ -7,29 +7,14 @@ namespace TraditionalToSimplified
     {
         static void Main(string[] args)
         {
-            Db_Tw_Service db_Tw_Service = new Db_Tw_Service();
-            Db_Event_Tw_Service db_Event_Tw_Service = new Db_Event_Tw_Service();
-            TESTDB dd = new TESTDB();
+            TraditionalToSimplified traditionalToSimplified = new TraditionalToSimplified();
             //計時
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            dd.ch();
-            //修改db_Tw至db_Cn資料庫
-            //db_Tw_Service.UpdateTableMy_Category_Tw();
-            //db_Tw_Service.UpdateTableMy_Country_Tw();
-            //db_Tw_Service.UpdateTableMy_Press_Tw();
-            //db_Tw_Service.UpdateTableMy_Product_Tw();
-            //db_Tw_Service.UpdateTableMy_Publisher_Tw();
-            //db_Tw_Service.UpdateTableMy_Region_Tw();
-            //db_Tw_Service.UpdateTableMy_Subtopic_Tw();
-            //db_Tw_Service.UpdateTableMy_Topic_Tw();
-            ////修改db_Event_Tw至db_Event_Cn資料庫
-            //db_Event_Tw_Service.UpdateTableMy_Event_Category_Tw();
-            //db_Event_Tw_Service.UpdateTableMy_Event_Press_Tw();
-            //db_Event_Tw_Service.UpdateTableMy_Event_Region_Tw();
-            //db_Event_Tw_Service.UpdateTableMy_Partnar_Category_Tw();
-
+            //執行修改繁體資料庫資料至簡體資料庫(繁體BIG5轉GB18030)
+            traditionalToSimplified.DbDataHandle();
+           
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
